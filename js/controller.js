f7064cc745711d82838800b5c7b9116a9aa1e2eb
Rory Comiskey <rorycomiskey@hotmail.com>
2017-10-03 15:14:02 +0000

    fourth commit

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,9 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        var video=$('#bg-video')[0];
-        var videoJ =$('#bg-video');
-        videoJ.on('ended',function(){
-        video.src("media/images/kol-bg.jpg"); 
+        var video = document.getElementById("bg-video");
+        video.playbackRate = 0.7; 
         
-    })
-});
+    });
+
 
         
