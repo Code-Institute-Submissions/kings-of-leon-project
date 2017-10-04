af7c9c8c5c41d0d51dac8851807afcee12b53989
Rory Comiskey <rorycomiskey@hotmail.com>
2017-10-04 16:43:57 +0000

    sixth commit

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,13 @@
-angular.module('RouteControllers', [])
+var KOLApp = angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
         var video = document.getElementById("bg-video");
-        video.playbackRate = 0.7; 
+        video.playbackRate = 0.6; 
+        })
         
-    });
+    KOLApp.controller('MediaController', function($scope) {
+        $(".nav li a").on("click", function() {
+        $(".nav li a").removeClass("active");   
+        $(this).addClass("active");
+    })
 
-
-        
+});
